2fc6121df6bbbf8057b867bd296edfb603bfdc73
Stephsas <stephanie0marchand@gmail.com>
2024-05-14 14:19:06 +0200

    maj

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 // Je récupère mon module express
 const express = require('express');
 
-const game = require('./my_modules/game');
+// const game = require('./my_modules/game');
+const games = require('./games.json')
 
 // Je crée une application express
 const app = express();
 
-// Importing a JSON file:
-//const game = require('./games.json');
-
-
 app.use(express.static('public'));
+
+app.locals.games = games;
 
 // Je vais préciser à express le moteur de rendu que je vais utiliser
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-
-    res.render('index', { game });
+    res.render('index');
 });
 
 app.get('/:gameName', (req, res) => {
     const gameName = req.params.gameName;
-console.log(gameName);
-    const gameFound = game.find((game) => {
+    console.log(gameName);
+    const gameFound = games.find((game) => {
         return game.name === gameName;
-
-
-
+     
     });
-
+      
+// Gérer le cas où aucun jeu correspondant n'est trouvé
     if (!gameFound) {
-        // Gérer le cas où aucun jeu correspondant n'est trouvé
+        
         res.status(404).send('Jeu non trouvé');
     } else {
         res.render(gameName, { game: gameFound });
