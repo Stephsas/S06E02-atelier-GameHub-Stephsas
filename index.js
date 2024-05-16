34c592025967b562fd8817c3e647d685d3e4b304
Stephsas <stephanie0marchand@gmail.com>
2024-05-16 10:30:57 +0200

    ajout

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const games = require('./games.json')
 // Je crée une application express
 const app = express();
 
+// Avant tous les autres middlewares, je vais rajouter celui qui va me permettre de logger les informations
+app.use((req, res, next) => {
+    const now = new Date();
+  
+  // Quand tu auras finis de faire ta requête, fait le console.log
+  // Ça permet de connaitre le code de statut HTTP de la réponse
+  req.on('end', () => {
+    console.log(`[${now.toISOString()} ${req.ip}] ${res.statusCode} ${req.path}`)
+  })
+
+  next()
+})
+
 app.use(express.static('public'));
+
+
 
 // faire une variable avec tous nos jeux
 
@@ -20,24 +35,21 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/:gameName', (req, res) => {
+// ON récupère le paramètre de notre URL
+app.get('/:gameName', (req, res, next) => {
     const gameName = req.params.gameName;
-    console.log(gameName);
-    const gameFound = games.find((game) => {
-        return game.name === gameName;
-
-    });
-
-    // Gérer le cas où aucun jeu correspondant n'est trouvé
+    const gameFound = games.find((game) => game.name === gameName);
     if (!gameFound) {
-
-        res.status(404).send('Jeu non trouvé');
+        next(); // Passer à la gestion des erreurs 404
     } else {
         res.render(gameName, { game: gameFound });
     }
+});
+// Middleware pour error 404
 
-})
-
+app.use((req, res, next) => {
+    res.status(404).send('Oups, page non trouvée');
+});
 
 // Je lance mon serveur
 app.listen(3001, () => {
